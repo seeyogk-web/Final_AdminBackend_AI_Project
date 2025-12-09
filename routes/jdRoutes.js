53b5a9ef91959a04ba2ff1960daf91d38274089f
saadakhtar23 <saad.akhtar@netfotech.in>
2025-12-09 17:04:19 +0530

    changes

--- routes/jdRoutes.js
+++ routes/jdRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {protect} from "../middlewares/auth.js";
+import { protect } from "../middlewares/auth.js";
 import { authorize } from '../middlewares/roles.js';
 import { createJD, createJDWithAI, getAllJds, getAllCandidates, addresumeToJD, getAllCandidatesAppliedToJD, getAssignedJDsByRMG, getAssignedOffersByRMG } from "../controllers/jdController.js";
 import { filterResumes } from "../controllers/aiResumeFilterController.js";
@@ -13,7 +13,9 @@ router.post("/:offerId", protect, authorize("HR"), createJD);
 // AI-powered JD creation
 router.post("/:offerId/ai", protect, authorize("HR"), createJDWithAI);
 router.post("/:jdId/filter-resumes", protect, authorize("HR"), filterResumes);
-router.get("/all-jd", protect, authorize("Admin"), getAllJds);
+router.get("/all-jd", protectCandidate, getAllJds);
+router.get("/all-jd-admin", protect, authorize("Admin"), getAllJds);
+router.get("/all-jd-hr", protect, authorize("HR"), getAllJds);
 router.get("/all-candidates", protect, authorize("HR", "Admin"), getAllCandidates);
 router.post("/:jdId/add-resume", protect, authorize("HR"), addresumeToJD);
 router.get("/:jdId/candidates", protect, authorize("HR"), getAllCandidatesAppliedToJD);
